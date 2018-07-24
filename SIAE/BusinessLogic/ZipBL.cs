512751d72e8b9df3b2f57563974f32b7b3dcd18b
ELVIN-MACBOOK\Elvin Salinas <ecse88@gmail.com>
2018-07-24 16:58:28 -0500

    Avanzando zip

--- SIAE/BusinessLogic/ZipBL.cs
+++ SIAE/BusinessLogic/ZipBL.cs
@@ -32,19 +32,27 @@ namespace BusinessLogic
                 foreach(DataRow dr in dt.Rows)
                 {
                     String rutaCarpeta = dr["VC_RUTA_CARPETA"].ToString();
-
+                    String nombreArchivo = dr["VC_VALOR_CADENA1"].ToString();
+                  
                     byte[] valorBinario = (byte[])dr["VB_VALOR_BINARIO"];
 
-                  //MemoryStream mBinario = new MemoryStream(valorBinario);
-                  //  Image img = (Bitmap)((new ImageConverter()).ConvertFrom(valorBinario));
+                    MemoryStream mBinario = new MemoryStream(valorBinario);
+
+                    //Image img = Image.FromStream(mbi);
                     String folder = IdNodo + rutaCarpeta;
                     String rutaCompleta = Path.Combine(ruta, folder);
+                    Directory.CreateDirectory(rutaCompleta);
+
+                    FileStream fs = new FileStream(rutaCompleta+"\\"+valorBinario, FileMode.Create, FileAccess.ReadWrite);
+                    fs.Write(valorBinario, 0, valorBinario.Length);
+                    fs.Flush();
+                    fs.Close();
                     ////img.Save(rutaCompleta);
-                    File.WriteAllBytes(rutaCompleta,valorBinario);
+                    // File.WriteAllBytes(rutaCompleta + "\\" +nombreArchivo ,valorBinario);
                 }
 
-                
-               
+
+
             }
             catch (Exception ex)
             {
